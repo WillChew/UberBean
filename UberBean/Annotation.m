e08c33f9d0f95a85043ce9f346f67e0d45ac9ac0
WillChew <williamchew95@gmail.com>
2018-06-22 15:38:01 -0400

    after setting up region

---
+++ UberBean/Annotation.m
@@ -0,0 +1,13 @@
+//
+//  Annotation.m
+//  UberBean
+//
+//  Created by Will Chew on 2018-06-22.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import "Annotation.h"
+
+@implementation Annotation
+
+@end
--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
 
-@interface ViewController () <CLLocationManagerDelegate>
+@interface ViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic,strong) CLLocationManager *locationManager;
 
@@ -23,8 +23,14 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    self.locationManager.distanceFilter = 20;
     self.locationManager.delegate = self;
     [self.locationManager requestWhenInUseAuthorization];
+    [self.locationManager requestLocation];
+    
+    self.mapView.delegate = self;
+    self.mapView.showsUserLocation = YES;
     
 }
 
@@ -33,6 +39,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [manager startUpdatingLocation];
+    }
+}
 
+
+
+
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    CLLocation *loc = locations[0];
+    [self.mapView setRegion:MKCoordinateRegionMake(loc.coordinate, MKCoordinateSpanMake(0.2, 0.2))];
+    NSLog(@"Got location");
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    if (error) {
+        NSLog(@"Error : %@" , error.localizedDescription);
+    }
+}
 
 @end
