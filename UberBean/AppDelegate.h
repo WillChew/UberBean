a7eb207ea89acb7441109b8f99f3a6a62171c43c
WillChew <williamchew95@gmail.com>
2018-06-22 14:34:52 -0400

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Will Chew on 2018-06-22.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
